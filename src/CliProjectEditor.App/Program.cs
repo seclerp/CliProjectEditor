c0cfe85de7bf852c25d10103a19f7df756cf7036
Andrew Rublyov <andrewrublyov99@gmail.com>
2021-07-15 19:54:17 +0300

    Implement PoC for project property setter using MSBuild SDK

---
+++ src/CliProjectEditor.App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Locator;
+
+namespace CliProjectEditor.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MSBuildLocator.RegisterDefaults();
+            Execute(args);
+        }
+
+        // Code should be called in separate method from where MSBuildLocator.RegisterDefaults is called
+        // see aka.ms/MSBuildLocator
+        private static void Execute(IReadOnlyList<string> args)
+        {
+            var project = ProjectCollection.GlobalProjectCollection.LoadProject(args[0]);
+            project.SetProperty(args[1], args[2]);
+            project.Save();
+            ProjectCollection.GlobalProjectCollection.UnloadProject(project);
+        }
+    }
+}
